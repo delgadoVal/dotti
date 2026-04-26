a10ef2af6126fd4647b3d660f97ea76ab839025f
delgadoVal <valedvillarce@hotmail.com>
2026-04-26 16:32:00 -0600

    added splash screen when app opens

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,10 +1,12 @@
 import 'package:dotti/views/home.dart';
 import 'package:dotti/views/meditation_page.dart';
+import 'package:dotti/views/splash_screen.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/',
+  initialLocation: '/splash',
   routes: [
+    GoRoute(path: '/splash', builder: (context, state) => const SplashScreen()),
     GoRoute(path: '/', builder: (context, state) => const Home()),
     GoRoute(
       path: '/meditation',
