3ba5c4ac742c9b37430b41e7d525e74250a6ba83
delgadoVal <valedvillarce@hotmail.com>
2026-04-26 12:46:12 -0600

    added theme

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+abstract final class AppColors {
+  static const Color background = Color(0xFFF2F2F2);
+  static const Color surface = Color(0xFFFFFFFF);
+  static const Color onSurface = Color(0xFF3C3C3C);
+
+  static const Color primary = Color(0xFF7C88FF);
+  static const Color primarySoft = Color(0xFFB2B9FB);
+  static const Color accent = Color(0xFFFFAE88);
+
+  static const Color textPrimary = Color(0xFF3C3C3C);
+  static const Color textOnPrimary = Color(0xFFFFFFFF);
+}
